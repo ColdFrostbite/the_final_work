9205a6ff1b56e9821dd708fe42f3493ea89748b1
Christina <62010621+ColdFrostbite@users.noreply.github.com>
2020-05-06 18:49:17 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummaCifer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Vstr;            
+            string R;
+           // int iNp;
+            int iSl; 
+           // int iS;
+            int SC;
+            int iCs;
+            char [] chVs;
+
+            Console.Write("Введите любые символы: ");
+            Vstr = Console.ReadLine();
+           // iNp = 1;
+            SC = 0;
+            iSl = Vstr.Length;                    
+            chVs = Vstr.ToCharArray();
+
+            for (int i = 0; i < iSl; i++)
+            {            
+                 if (char.IsDigit(chVs[i]))
+                 {
+                   R = Vstr.Substring(i, 1);
+                   iCs = Convert.ToInt32(R);
+                   SC = SC+iCs;
+                 }
+            }
+            Console.WriteLine("Сумма цифр из строки ровна: " + SC);
+            Console.ReadLine();
+        }
+    }
+}
